790bb24c7732afd7266fdd15858449775121a38d
ksonawat007 <ssks81285@gmail.com>
2022-07-15 21:56:49 +0530

    Completed Sidebar. Header Next

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Header.css"
+
+const Header = () => {
+    return (
+        <header className="Topbar_MainContainer">Header</header>
+    )
+}
+
+export default Header
