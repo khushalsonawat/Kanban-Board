ac6af6fb0cd8d3695fc9a1b94004b67e4abbada7
ksonawat007 <ssks81285@gmail.com>
2022-07-18 21:45:38 +0530

    A few styling changes

--- src/Components/Column/Column.js
+++ src/Components/Column/Column.js
@@ -17,7 +17,7 @@ const Column = (props) => {
         <div className="ListCard_MainContainer">
             <div className="ListCard_TitleWrapper" >
                 <h3 className="ListCard_Title">{props.title}</h3>
-                <span><MoreHoriz /></span>
+                <span className='More'><MoreHoriz /></span>
             </div>
             <div className='list' onDragOver={props.onDragOver} onDrop={props.onDrop} >
                 {cards}
